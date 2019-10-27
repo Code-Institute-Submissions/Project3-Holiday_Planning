29370e7e26e371401fe4c3f5807da4c94a9a74aa
Ubuntu <ubuntu@ip-172-31-45-192.us-east-2.compute.internal>
2019-10-27 03:56:28 +0000

    add external css and js link

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
+  
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId 
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'holiday_planning'
+app.config["MONGO_URI"] = 'mongodb+srv://root:es1234@cluster0-dio8w.mongodb.net/holiday_planning?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
 
 
 @app.route('/')
-def hello():
-    return 'Hello World ...again'
+@app.route('/get_tasks')
+def get_tasks():
+    return render_template("tasks.html", tasks=mongo.db.tasks.find())
 
 
 if __name__ == '__main__':
