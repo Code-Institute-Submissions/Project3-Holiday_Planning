0e70611ca76f60f31b73ac2804f14e5a5f820fd1
Ubuntu <ubuntu@ip-172-31-45-192.us-east-2.compute.internal>
2019-10-27 11:33:14 +0000

    create addTask.html with select option and submit button

--- app.py
+++ app.py
@@ -7,15 +7,29 @@ from bson.objectid import ObjectId
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'holiday_planning'
 app.config["MONGO_URI"] = 'mongodb+srv://root:es1234@cluster0-dio8w.mongodb.net/holiday_planning?retryWrites=true&w=majority'
-
 mongo = PyMongo(app)
 
 
+# this function fetch all our data in mongodb pass it back to tasks.html
 @app.route('/')
 @app.route('/get_tasks')
 def get_tasks():
     return render_template("tasks.html", tasks=mongo.db.tasks.find())
+    
 
+# this function fetch all our category in mongodb and pass it back to addTask.htmk <select> 
+@app.route('/add_task')
+def add_task():
+    return render_template('addtask.html', category=mongo.db.category.find())
+    
+ 
+# this function will post new data to mondodb when click on the submit button in addTask.html. We convert the form to dictionary so can be understood by mongo.    
+@app.route('/insert_task', methods=['POST'])
+def insert_task():
+    tasks = mongo.db.tasks
+    tasks.insert_one(request.form.to_dict())
+    return redirect(url_for('get_tasks'))
+    
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
