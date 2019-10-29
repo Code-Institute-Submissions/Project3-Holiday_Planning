cff485a8f3aa3f7c524fc948916b7642e5656382
Ubuntu <ubuntu@ip-172-31-45-192.us-east-2.compute.internal>
2019-10-29 06:07:34 +0000

    category.html created. Function get_category also created in app.py

--- app.py
+++ app.py
@@ -10,30 +10,50 @@ app.config["MONGO_URI"] = 'mongodb+srv://root:es1234@cluster0-dio8w.mongodb.net/
 mongo = PyMongo(app)
 
 
-# Function to display landing page (Sam's code)
+# Function to display landing page
 @app.route('/')
 def index():
     return render_template("tasks.html", tasks=mongo.db.tasks.find())
     
 
-# this function fetch all our data in mongodb pass it back to tasks.html
+# Function to fetch all our data in mongodb pass it back to tasks.html
 @app.route('/get_tasks')
 def get_tasks():
-    return render_template("tasks.html", tasks=mongo.db.tasks.find())
+    return render_template("tasks.html", 
+                            tasks=mongo.db.tasks.find())
     
 
-# this function fetch all our category in mongodb and pass it back to addTask.htmk <select> 
+# Function to fetch all our category in mongodb and pass it back to addTask.html <select> 
 @app.route('/add_task')
 def add_task():
-    return render_template('addTask.html', category=mongo.db.category.find())
+    return render_template('addTask.html', 
+                            category=mongo.db.category.find())
     
  
-# this function will post new data to mondodb when click on the submit button in addTask.html. We convert the form to dictionary so can be understood by mon    
+# Function will post new data to mondodb when click on the submit button in addTask.html. We convert the form to dictionary so can be understood by mon    
 @app.route('/insert_task', methods=['POST'])
 def insert_task():
     tasks = mongo.db.tasks
     tasks.insert_one(request.form.to_dict())
     return redirect(url_for('get_tasks'))
+    
+
+
+# CATEGORY
+
+# Function to fetch all our data in mongodb pass it back to category.html
+@app.route('/get_category')
+def get_category():
+    return render_template("category.html", 
+                            category=mongo.db.category.find())
+    
+    
+# Function to fetch all our category in mongodb and pass it back to addCategory.html <select> 
+@app.route('/add_category')
+def add_category():
+    return render_template('addCategory.html')     
+
+    
     
 
 if __name__ == '__main__':
